b4fb19bb0863453379c5c2e1c313c7c5921bea83
Sylvain Filoni <fffiloni@users.noreply.huggingface.co>
2023-01-12 17:35:46 +0000

    Update app.py

--- app.py
+++ app.py
@@ -175,7 +175,7 @@ with gr.Blocks(css=css) as demo:
     
         input_img = gr.Image(type="filepath", elem_id="input-img")
         with gr.Row():
-            track_duration = gr.Slider(minimum=20, maximum=120, value=30, step=5, label="🎅 Track duration", elem_id="duration-inp")
+            track_duration = gr.Slider(minimum=20, maximum=120, value=30, step=5, label="Track duration", elem_id="duration-inp")
             gen_intensity = gr.Dropdown(choices=["low", "medium", "high"], value="high", label="Complexity")
             gen_mode = gr.Radio(label="mode", choices=["track", "loop"], value="track")
         generate = gr.Button("Generate Music from Image")
